8bf0917cf6c92b4512f387283c6b456e07282f60
Andrea Pregnolato <andrea.pregnolato@nearform.com>
2026-06-09 12:25:56 +0200

    fix: enable ssl on postgres

--- ogcio/index.js
+++ ogcio/index.js
@@ -1,5 +1,5 @@
-const unleash = require('unleash-server');
-const oidcAuthHook = require('./ogcio/oidc-auth-hook.js');
+const unleash = require("unleash-server");
+const oidcAuthHook = require("./ogcio/oidc-auth-hook.js");
 
 const {
     POSTGRES_USER,
@@ -8,22 +8,30 @@ const {
     POSTGRES_PORT,
     POSTGRES_DB_NAME,
     LOG_LEVEL,
+    POSTGRES_SSL,
 } = process.env;
+
+const ssl = POSTGRES_SSL
+    ? {
+          rejectUnauthorized: false,
+          ca: fs.readFileSync("ogcio/global-bundle.pem"),
+      }
+    : false;
 
 const options = {
     db: {
-        user: POSTGRES_USER ?? 'unleash',
-        password: POSTGRES_PASSWORD ?? 'unleash',
-        host: POSTGRES_HOST ?? 'localhost',
+        user: POSTGRES_USER ?? "unleash",
+        password: POSTGRES_PASSWORD ?? "unleash",
+        host: POSTGRES_HOST ?? "localhost",
         port: Number(POSTGRES_PORT ?? 5432),
-        database: POSTGRES_DB_NAME ?? 'unleash',
-        ssl: false,
+        database: POSTGRES_DB_NAME ?? "unleash",
+        ssl: ssl,
     },
     authentication: {
-        type: 'custom',
+        type: "custom",
         customAuthHandler: oidcAuthHook,
     },
-    logLevel: LOG_LEVEL ?? 'info',
+    logLevel: LOG_LEVEL ?? "info",
 };
 
 unleash.start(options);
